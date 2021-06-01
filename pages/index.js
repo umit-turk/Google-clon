3b98ebed612a58ee87043f37112a357c402a2a6a
Umit Turk <uyt.turk@gmail.com>
2021-06-01 23:31:39 +0300

    body

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,62 @@
-import Head from 'next/head'
-import Avatar from '../components/Avatar'
+import Head from "next/head";
+import Avatar from "../components/Avatar";
+import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
+import {  SearchIcon } from "@heroicons/react/outline";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <Head>
         <title>Google</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-     
 
-     {/* Header */}
-     <header className="flex w-full p-5 justify-between text-sm text-gray-700">
-       {/* left */}
-       <div className="flex space-x-4">
-        <p className="link">About</p>
-        <p className="link">Store</p>
-       </div>
+      {/* Header */}
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+        {/* left */}
+        <div className="flex space-x-4 items-center">
+          <p className="link">About</p>
+          <p className="link">Store</p>
+        </div>
 
-       {/* right */}
+        {/* right */}
 
-       <div className="flex space-x-4 items-center">
-        <p className="link">Gmail</p>
-        <p className="link">Images</p>
+        <div className="flex space-x-4 items-center">
+          <p className="link">Gmail</p>
+          <p className="link">Images</p>
 
-        {/* Icon */}
+          {/* Icon */}
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
 
-        {/* Avatar */}
-        <Avatar url="https://lh3.googleusercontent.com/ogw/ADGmqu-pPBzu3iEXf-iWrNKRSJYZZBS2uUP3rkVnXpNT=s32-c-mo" />
+          {/* Avatar */}
+          <Avatar url="https://lh3.googleusercontent.com/ogw/ADGmqu-pPBzu3iEXf-iWrNKRSJYZZBS2uUP3rkVnXpNT=s32-c-mo" />
+        </div>
+      </header>
 
-        
+      {/* body */}
+      <form className="flex flex-col items-center mt-44 flex-grow">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1920px-Google_2015_logo.svg.png"
+          height={100}
+          width={300}
+        />
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input type="text" className="flex-grow  focus:outline-none" />
+          <MicrophoneIcon className="h-5" />
+        </div>
 
-       </div>
-     </header>
+      <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+        <button className="btn">Google Search</button>
 
-     {/* body */}
+        <button className="btn">I'm Feeling Lucky</button>
+      </div>
+
+      </form>
+
 
       {/* footer */}
     </div>
-  )
+  );
 }
