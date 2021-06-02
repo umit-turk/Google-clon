962c7b54474de361bef6eabe98a9f324fc4a5272
Umit Turk <uyt.turk@gmail.com>
2021-06-02 17:56:49 +0300

    header option(s) component

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,6 +1,6 @@
-function Avatar({ url }) {
+function Avatar({ url, className }) {
   return (
-    <img loading="lazy" src={url} className=" cursor-pointer transition duration-150 transform hover:scale-110 rounded-full" alt="profile pic" />
+    <img loading="lazy" src={url} className={ `cursor-pointer transition duration-150 transform hover:scale-110 rounded-full ${className}` } alt="profile pic" />
   );
 }
 
